81f5a6577f60785e1fa8dde080c72b3c2582582f
Mayank Saini <mayankapply9166@gmail.com>
2025-02-12 23:30:48 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
    
     const fetchImages = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/images", {
+        const response = await axios.get("https://mayank-6ry9.onrender.com/api/images", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("Token")}`, // Ensure a valid token is stored
           },
